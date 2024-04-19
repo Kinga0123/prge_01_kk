323222b0367858c42cece628b86052bc5025b668
Mateusz10099 <149382121+Mateusz10099@users.noreply.github.com>
2024-04-19 12:48:06 +0200

    feat landing page

--- src/componets/home/Home.js
+++ src/componets/home/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
 import "./Home.css";
+import plan_wat from "./Zrzut ekranu 2024-04-12 102300.png";
+
 function Home({ text }) {
-  return <div className="home">hello {text}</div>;
+  return (
+    <div className="home">
+      <div className="home_top">
+        <div className="home_top_about">O projekcie</div>
+      </div>
+      <div className="home_bottom">
+        <div className="home_left">
+          <img className="plan_wat" src={plan_wat} alt="logo" />
+        </div>
+
+        <div className="home_right">
+          <div className="home_right_title">GEOPORTAL</div>
+
+          <div className="home_right_subtitle">
+            Strona internetowa z interaktywną mapą
+          </div>
+
+          <button className="home_right_btn">START</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
