fa478e25576fb41eed7c1e01a67b4a02beccb962
Kinga0123 <149382131+Kinga0123@users.noreply.github.com>
2024-06-07 11:08:20 +0200

    wip: add wfs management

--- src/components/map/Map copy.js
+++ src/components/map/Map copy.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Map.css";
+import "./map.css";
 import {
   LayersControl,
   MapContainer,
@@ -9,30 +9,18 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import axios from "axios";
-
 function Map() {
   const [wojewodztwa, setwojewodztwa] = useState(null);
-
-  const makePopup = (feature, layer) => {
-    if (feature.properties) {
-      // console.log(feature.properties);
-      layer.bindPopup(`
-      <h1>Dane województwa</h1>
-      <strong>Nazwa:</strong> ${feature.properties.jpt_nazwa_}</br>
-      <strong>Powierzchnia:</strong>${feature.properties.jpt_powier} m2 </br>
-      <img src=${feature.properties.img_source} alt="Lamp" width="32" height="32"/>`);
-    }
-  };
-
   useEffect(() => {
-    // console.log("aaa");
+    console.log("aaa");
     const getData = () => {
       axios
         .get(
-          "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Agranice_wojewodztw_db_4326&maxFeatures=50&outputFormat=application%2Fjson"
+          "http://127.0.0.1:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Agranice_wojewodztw&maxFeatures=50&outputFormat=application%2Fjson"
+          //"https://jsonplaceholder.typicode.com/posts/1"
         )
         .then((dane) => {
-          // console.log(dane);
+          console.log(dane);
           setwojewodztwa(dane.data);
         });
     };
@@ -42,27 +30,23 @@ function Map() {
     <div className="map">
       <MapContainer center={[52.2322222, 21.0]} zoom={10}>
         <LayersControl>
-          <LayersControl.BaseLayer name="OSM">
+          <LayersControl.BaseLayer checked name="OSM">
             <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Google">
+          <LayersControl.BaseLayer checked name="Google">
             <TileLayer url="http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Google Satelite">
+          <LayersControl.BaseLayer checked name="Google Satelite">
             <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Granice województw DB">
+          <LayersControl.BaseLayer checked name="Granice województw DB">
             <WMSTileLayer
               layers="granice_wojewodztw"
               url="http://127.0.0.1:8080/geoserver/prge/wms"
             />
           </LayersControl.BaseLayer>
           <LayersControl.Overlay checked name="Granice województw DB WFS">
-            {wojewodztwa ? (
-              <GeoJSON data={wojewodztwa} onEachFeature={makePopup} />
-            ) : (
-              ""
-            )}
+            {wojewodztwa ? <GeoJSON data={wojewodztwa} /> : ""}
           </LayersControl.Overlay>
         </LayersControl>
       </MapContainer>
